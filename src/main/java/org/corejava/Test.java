817b8760f5d65af9876b178170b50b699d45b2e2
Yeung <@Yeung>
2025-04-04 19:35:35 +0900

    macbook 4-4 1935

--- src/main/java/org/corejava/Test.java
+++ src/main/java/org/corejava/Test.java
@@ -7,8 +7,11 @@ import static java.lang.System.out;
 
 public class Test {
     public static void main(String[] args) {
-        String myStr = "       Hello World!        ";
-        System.out.println(myStr);
-        System.out.println(myStr.trim());
+        Integer a = -128;
+        Integer b = -128;
+        Integer c = 127;
+        Integer d = 127;
+        System.out.println(a == b);
+        System.out.println(c == d);
     }
 }
